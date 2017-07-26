cede93ec6cfec23e19d472c710bf666ad0ceec8b
422Mac01 <422mac01@422Mac01s-iMac.local>
2017-07-26 01:18:46 -0700

    Initial Commit

---
+++ Easy Authen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Authen
+//
+//  Created by 422Mac01 on 26/7/60.
+//  Copyright © พ.ศ. 2560 jane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
